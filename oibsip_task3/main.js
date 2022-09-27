1ae1fd88b9a6ed83b894c33892b90e3a2c89ed3e
Saptarshi Banerjee <90561422+TheSaptarshiBanerjee@users.noreply.github.com>
2022-09-27 11:39:22 +0530

    task 3

---
+++ oibsip_task3/main.js
@@ -0,0 +1,29 @@
+const inputs = document.querySelectorAll(".panel input");
+const c = document.querySelector("#celsius");
+const f = document.querySelector("#fahrenheit");
+const k = document.querySelector("#kelvin");
+
+
+inputs.forEach(input => {
+    input.addEventListener("input", e => {
+        const unit = e.target.id;
+        const v = parseInt(e.target.value);
+
+        if(unit === "celsius"){
+            f.value = parseFloat((v * 1.8) +32).toFixed(4) * 1;
+            k.value = parseFloat(v + 273.15).toFixed(4) * 1;
+
+        }
+        else if(unit === "fahrenheit") {
+            c.value = parseFloat((v - 32) * 5 / 9).toFixed(4) * 1;
+            k.value = parseFloat(((v - 32) * 5 / 9)+ 273.15).toFixed(4) * 1;
+        }
+
+        else if(unit === "kelvin")
+        {
+            c.value = parseFloat(v - 273.15).toFixed(4)* 1;
+            f.value = parseFloat((v - 273.15) * 9 / 5 +32).toFixed(4) * 1;
+        
+        }
+    });
+});
